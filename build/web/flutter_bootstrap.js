54e5f0f391daf7e0a093e9d3089c8facc76491f8
sushiosushi <soshi_takemoto@reazon.jp>
2024-08-23 18:24:08 +0900

    fixed

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2029688968"
+    serviceWorkerVersion: "3627181769"
   }
 });
