2b5bbc6922fd8a1ede434fb26af0eb6a11fae6cd
sushiosushi <soshi_takemoto@reazon.jp>
2024-08-23 15:30:40 +0900

    made

---
+++ deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+
+webDeploy() {
+  echo "🍏 Web build started" 
+
+  flutter build web --base-href /$REPOSITORY_NAME/
+  # mv build/web ./docs/.
+
+  ## move all files in build/web to docs/
+  rsync -av --remove-source-files build/web/ docs/
+  
+  git add .
+
+  open -n -a "Google Chrome.app" --args https://sushiosushi.github.io/zapland_web/$REPOSITORY_NAME  --profile-directory="Profile 3"
+
+  echo "🍏 Web build completed"
+}
+
+
+# Increment version
+perl -i -pe 's/^(version:\s+\d+\.\d+\.\d+\+)(\d+)$/$1.($2+1)/e' pubspec.yaml
+
+source .env
+# Deploy
+webDeploy &
+
+wait
+echo "A build finished 🎉🎉🎉"
