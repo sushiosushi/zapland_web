450ec1d6cce8e7c9471b54fb2d3ce00f0fb93c1d
sushiosushi <soshi_takemoto@reazon.jp>
2024-09-10 17:21:09 +0900

    changed

--- lib/helpers/urlLauncher.dart
+++ lib/helpers/urlLauncher.dart
@@ -3,7 +3,7 @@ import 'package:url_launcher/url_launcher.dart';
 Future<void> launchUrlFromWeb(Uri url) async {
   if (await canLaunchUrl(url)) {
     launchUrl(url,
-        mode: LaunchMode.platformDefault, webOnlyWindowName: '_blank');
+        mode: LaunchMode.platformDefault, webOnlyWindowName: '_self');
   } else {
     print('Cannot launch url: $url');
   }
