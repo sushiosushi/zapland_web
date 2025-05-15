404b230d5054f9fd21adcc9280b39df6dad08c2b
sushiosushi <soshi_takemoto@reazon.jp>
2025-05-15 21:29:37 +0900

    fixed bug (if isInZapshot, call API)

--- lib/helpers/userAgent.dart
+++ lib/helpers/userAgent.dart
@@ -1,14 +1,17 @@
 import 'package:device_info_plus/device_info_plus.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import 'secretCode.dart';
+
 final isInZapshotWebViewProvider = FutureProvider<bool>((ref) async {
   DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
   WebBrowserInfo webBrowserInfo = await deviceInfo.webBrowserInfo;
 
   if (webBrowserInfo.userAgent != null &&
       webBrowserInfo.userAgent!.contains('zapshot')) {
+    ref.read(secretCodeProvider.notifier).loadOrIssueCode();
+
     return true;
   }
-
   return false;
 });
